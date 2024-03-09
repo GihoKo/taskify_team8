7ad0387e14343ef5a6333ac41fdddfd9172c1a3d
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2024-03-09 17:42:49 +0900

    Package: 프로젝트 라이브러리 변경사항 적용
    1. 추가
    - tanstack query
    - zustand
    - styled-components
      - next config
      - css reset
    
    2. 제거
    - tailwind 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  compiler: {
+    styledComponents: {
+      cssProp: true,
+      displayName: true,
+      ssr: true,
+    },
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
 export default nextConfig;
