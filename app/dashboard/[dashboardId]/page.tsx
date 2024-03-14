1028b56bb2f4031dcb088962d74282798c0f5b10
Vegatality <vegatality@gmail.com>
2024-03-14 18:31:07 +0900

    Feat: Dashboard 페이지 공용 layout 생성
    1. dashboard/{dashboardId} 페이지와 dashboard/{dashboardId}/edit 에서 공용으로 쓰이는 네비게이션 바 layout으로 이동

--- app/dashboard/[dashboardId]/page.tsx
+++ app/dashboard/[dashboardId]/page.tsx
@@ -1,37 +1,30 @@
 import CardColumn from './_components/atoms/CardColumn';
 import ColumnContainer from './_components/atoms/ColumnContainer';
 import ColumnList from './_components/atoms/ColumnList';
-import Page from './_components/atoms/Page';
 import CardAppendRectangle from './_components/molecules/CardAppendRectangle';
 import ColumnAppenderRectangle from './_components/molecules/ColumnAppenderRectangle';
 import Card from './_components/organisms/Card';
 import ColumnHeader from './_components/organisms/ColumnHeader';
-import DashboardNav from './_components/organisms/DashboardNav';
 
 const DashboardPage = () => {
   return (
     <>
-      <Page>
-        <DashboardNav />
-        <ColumnList>
-          {[1, 2, 3, 4].map((v) => (
-            <ColumnContainer key={v}>
-              <ColumnHeader />
-              <CardColumn>
-                <CardAppendRectangle />
-                <Card />
-                <Card />
-                <Card />
-              </CardColumn>
-            </ColumnContainer>
-          ))}
-          <ColumnContainer isLastColumn>
-            <ColumnAppenderRectangle />
+      <ColumnList>
+        {[1, 2, 3].map((v) => (
+          <ColumnContainer key={v}>
+            <ColumnHeader />
+            <CardColumn>
+              <CardAppendRectangle />
+              <Card />
+              <Card />
+              <Card />
+            </CardColumn>
           </ColumnContainer>
-        </ColumnList>
-      </Page>
-      {/* <Cogwheel /> */}
-      {/* <Cogwheel fixedSize='2rem' /> */}
+        ))}
+        <ColumnContainer isLastColumn>
+          <ColumnAppenderRectangle />
+        </ColumnContainer>
+      </ColumnList>
     </>
   );
 };
