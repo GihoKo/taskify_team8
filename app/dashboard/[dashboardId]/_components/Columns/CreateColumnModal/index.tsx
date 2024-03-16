0b5cde32be082465ee812feacf183f41d0941e3f
eundin <escience1202@gmail.com>
2024-03-16 19:55:12 +0900

    Feat: 컬럼 관리 모달 디자인 구현

--- app/dashboard/[dashboardId]/_components/Columns/CreateColumnModal/index.tsx
+++ app/dashboard/[dashboardId]/_components/Columns/CreateColumnModal/index.tsx
@@ -1,7 +1,50 @@
 'use client';
 
-import ColumnModal from '../commons/ColumnModal';
+import styled from 'styled-components';
+
+import { mediaBreakpoint } from '@styles/mediaBreakpoint';
+
+import ColumnButton from '../commons/ColumnButton';
+import ColumnInput from '../commons/ColumnInput';
+import CreateModalTitle from '../commons/ColumnModalTitile';
 
 export default function CreateColumnModal() {
-  return <ColumnModal title='새 칼럼 생성' text1='취소' text2='생성' />;
+  return (
+    <S.CreateColumnBox>
+      <CreateModalTitle title='새 컬럼 생성' />
+      <ColumnInput />
+      <S.ColumnButtonContainer>
+        <ColumnButton text='취소' />
+        <ColumnButton text='생성' />
+      </S.ColumnButtonContainer>
+    </S.CreateColumnBox>
+  );
 }
+
+const S = {
+  CreateColumnBox: styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 2.8rem 2rem;
+    width: 32.7rem;
+    height: 24.1rem;
+
+    border-radius: 0.8rem;
+    background-color: ${({ theme }) => theme.color.white_FFFFFF};
+
+    @media ${mediaBreakpoint.tablet} {
+      width: 54rem;
+      height: 27.6rem;
+      padding: 3.2rem 2.8rem;
+    }
+  `,
+  ColumnButtonContainer: styled.div`
+    display: flex;
+    gap: 1.1rem;
+
+    @media ${mediaBreakpoint.tablet} {
+      gap: 1.2rem;
+      justify-content: flex-end;
+    }
+  `,
+};
--- app/dashboard/[dashboardId]/_components/Columns/UpdateColumnModal/index.tsx
+++ app/dashboard/[dashboardId]/_components/Columns/UpdateColumnModal/index.tsx
@@ -1,7 +1,74 @@
 'use client';
 
-import ColumnModal from '../commons/ColumnModal';
+import styled from 'styled-components';
+
+import { mediaBreakpoint } from '@styles/mediaBreakpoint';
+
+import ColumnButton from '../commons/ColumnButton';
+import ColumnInput from '../commons/ColumnInput';
+import CreateModalTitle from '../commons/ColumnModalTitile';
 
 export default function UpdateColumnModal() {
-  return <ColumnModal title='칼럼 관리' text1='취소' text2='변경' />;
+  return (
+    <S.DeleteColumnModalBox>
+      <CreateModalTitle title='컬럼 관리' />
+      <ColumnInput />
+      <S.ColumnButtonContainer>
+        <S.ColumnDeleteButton>삭제하기</S.ColumnDeleteButton>
+        <S.ColumnButtonsWrap>
+          <ColumnButton text='취소' />
+          <ColumnButton text='변경' />
+        </S.ColumnButtonsWrap>
+      </S.ColumnButtonContainer>
+    </S.DeleteColumnModalBox>
+  );
 }
+
+const S = {
+  ColumnButtonsWrap: styled.div`
+    display: flex;
+    gap: 1.1rem;
+
+    grid-area: buttons;
+
+    @media ${mediaBreakpoint.tablet} {
+      gap: 1.2rem;
+      justify-content: flex-end;
+    }
+  `,
+  ColumnDeleteButton: styled.span`
+    font-size: 1.4rem;
+    color: red;
+    opacity: 0.5;
+    grid-area: delete;
+    cursor: pointer;
+  `,
+  ColumnButtonContainer: styled.div`
+    display: grid;
+    gap: 1.6rem;
+    grid-template-areas:
+      'delete'
+      'buttons';
+    @media ${mediaBreakpoint.tablet} {
+      grid-template-areas: 'delete buttons';
+      align-items: flex-end;
+    }
+  `,
+
+  DeleteColumnModalBox: styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 2.8rem 2rem;
+    width: 32.7rem;
+    height: 27.4rem;
+
+    border-radius: 0.8rem;
+    background-color: ${({ theme }) => theme.color.white_FFFFFF};
+
+    @media ${mediaBreakpoint.tablet} {
+      width: 54rem;
+      height: 27.6rem;
+      padding: 3.2rem 2.8rem;
+    }
+  `,
+};
