8d8c0cefdfbeb33c988fc2bfe701a238ef223846
Vegatality <vegatality@gmail.com>
2024-03-19 00:55:46 +0900

    Refactor: useModal 프롭 타입 개선
    1. 두 번째 인자에 null 받을 수 있게 변경
    2. 두 번째 인자 ModalHandler 타입 가질 수 있도록 변경

--- hooks/useToggle.ts
+++ hooks/useToggle.ts
@@ -1,5 +1,8 @@
+'use client';
+
 import { Reducer, useReducer } from 'react';
 
+// TODO: 여기도 client 지시문 필요할까?
 const useToggle = <T>(initial: T) => {
   const [state, toggleState] = useReducer<Reducer<boolean, void>, T>((prev) => !prev, initial, Boolean);
 
