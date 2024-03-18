cabdc20dc565ec24e62b7a8ee92eec043f2ed0f6
giho <giho7427@daum.net>
2024-03-19 02:15:31 +0900

    refactor: 엑세스 토큰 env 파일로 관리

--- app/mydashboard/_components/apis/instance/index.ts
+++ app/mydashboard/_components/apis/instance/index.ts
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 const URL_DOMAIN = 'https://sp-taskify-api.vercel.app/3-8';
 
-const ACCESS_TOKEN_1 =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTI4NywidGVhbUlkIjoiMy04IiwiaWF0IjoxNzEwNzY5OTQxLCJpc3MiOiJzcC10YXNraWZ5In0.l-bCi4NEjNIYp55xVj8c4c7MGVWDYmqyWR1MKIgQdrM';
-
-// const ACCESS_TOKEN_2 =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMxNiwidGVhbUlkIjoiMy04IiwiaWF0IjoxNzEwNzczMDgwLCJpc3MiOiJzcC10YXNraWZ5In0.YMViqJVTf-cMftLeQZZOnBKivp6w2bmps4ego1Yx2Mo';
-
 export const defaultInstance = axios.create({
   baseURL: URL_DOMAIN,
 });
@@ -18,7 +12,7 @@ export const instanceAddedAccessToken = axios.create({
 });
 
 instanceAddedAccessToken.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${ACCESS_TOKEN_1}`;
+  config.headers.Authorization = `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_1}`;
 
   return config;
 });
