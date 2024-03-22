fd20ecfdf705464dbbfb26ec8ded6f5de5f6757b
Vegatality <vegatality@gmail.com>
2024-03-23 04:00:38 +0900

    Refactor: cards 쿼리키 이름 수정

--- queries/keys/cardsKeys.ts
+++ queries/keys/cardsKeys.ts
@@ -2,8 +2,7 @@ import { getCardList, GetCardListParams } from '@apis/cards/getCardList';
 
 export const cardsKeys = {
   masterKey: () => ['cards'] as const,
-  cardList: (columnId: number) => [...cardsKeys.masterKey(), 'cardList', columnId] as const,
-  infiniteCardList: (columnId: number) => [...cardsKeys.masterKey(), columnId, 'infiniteCardList'] as const,
+  cardList: (columnId: number) => [...cardsKeys.masterKey(), columnId, 'cardList'] as const,
 };
 
 export const cardsQueryOptions = {
@@ -12,13 +11,6 @@ export const cardsQueryOptions = {
   }),
   cardList: ({ columnId, size }: GetCardListParams) => ({
     queryKey: cardsKeys.cardList(columnId),
-    // 0 이면 null
-    queryFn: ({ pageParam }: { pageParam: number }) => getCardList({ columnId, cursorId: pageParam || null, size }),
-    gcTime: 1000 * 60 * 5, // 5분
-    staleTime: 1000 * 60 * 1, // 1분
-  }),
-  infiniteCardList: ({ columnId, size }: GetCardListParams) => ({
-    queryKey: cardsKeys.infiniteCardList(columnId),
     // 0 이면 null
     queryFn: ({ pageParam }: { pageParam: number }) => getCardList({ columnId, cursorId: pageParam || null, size }),
     gcTime: 1000 * 60 * 5, // 5분
