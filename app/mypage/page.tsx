469b3326b62017b8dbda6e2f6caf222eea208ef7
giho <giho7427@daum.net>
2024-03-25 08:28:29 +0900

    fix: 이미지 업로드 초기화 버그 수정

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -106,6 +106,13 @@ function MyPage() {
 
   // -- 이미지 / 닉네임 변경 시작
   const fetchProfileImage = useCallback(async () => {
+    // 긴급 처방
+    // 이미지가 기본 이미지가 아닐 때 서버에서 이미지를 가져오지 않는다.
+    // 따라서 이미지가 기본 이미지일 때 서버에서 이미지를 가져온다
+    if (previewUrl !== '/images/icons/profile-add-filledViolet_5544DA-w182-h183.svg') {
+      return;
+    }
+
     try {
       const response = await axiosToken.get('users/me');
 
@@ -165,11 +172,12 @@ function MyPage() {
     }
   };
 
+  // 긴급 처방
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
 
-      if (file && file.type.startsWith('image/')) {
+      if (file) {
         const imageUrl = await uploadImage(file);
 
         if (imageUrl) {
@@ -186,12 +194,14 @@ function MyPage() {
     }
   };
 
-  const handleDeleteImage = () => {
+  const handleDeleteImage = async () => {
     setProfileValue((prev) => ({
       ...prev,
       profileImageUrl: null,
     }));
     setPreviewUrl('/images/icons/profile-add-filledViolet_5544DA-w182-h183.svg');
+    // 긴급 처방
+    await handleChangeProfile({ nickname: user.nickname, profileImageUrl: null });
   };
 
   useEffect(() => {
