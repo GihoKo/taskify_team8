8ddc12d38a9e100820d123469d0d3a2e026ba833
eundin <escience1202@gmail.com>
2024-03-24 17:18:41 +0900

    chore: 타입 오류 수정

--- apis/cards/postCardImage.ts
+++ apis/cards/postCardImage.ts
@@ -1,6 +1,6 @@
 import { axiosToken } from '@apis/instance/axiosToken';
 
-export const postCreateCardImage = async (columnId: number, image) => {
+export const postCreateCardImage = async (columnId: number, image: FormData) => {
   const response = await axiosToken.post(`/columns/${columnId}/card-image`, image, {
     headers: {
       'Content-Type': 'multipart/form-data',
