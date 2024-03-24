0f64661eb070a83ee88e3408b62dce4576c61d0d
giho <giho7427@daum.net>
2024-03-24 22:52:22 +0900

    refactor: 필요없는 api 폴더 삭제

--- apis/dashboards/postInvitation.ts
+++ apis/dashboards/postInvitation.ts
@@ -12,7 +12,5 @@ export const postInvitation = async (
 ) => {
   const res = await axiosToken.post<PostInvitationResponse>(`/dashboards/${dashboardId}/invitations`, body);
 
-  console.log(res);
-
   return res;
 };
